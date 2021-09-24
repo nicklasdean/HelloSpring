ba7a5dec10d4ebe7dac473f85fcc7605ad59cc4d
nicklasdean <61466422+nicklasdean@users.noreply.github.com>
2021-09-24 10:13:42 +0200

    first

--- src/main/java/com/example/hellospring/controllers/HelloController.java
+++ src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.hellospring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,22 @@ public class HelloController {
     @GetMapping("/first-mapping")
     public String firstMapping(){
         return "<b>Hello World<b>";
+    }
+
+    @GetMapping("/second-mapping")
+    public String secondMapping(){
+        return "This is the second mapping";
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "Welcome to my Spring Application";
+    }
+
+    @GetMapping("/parameter")
+    public String parameter(@RequestParam String input){
+        //For this to work:
+        //localhost:8080/parameter?input=HelloWorld
+        return input + " " + "end of string";
     }
 }
